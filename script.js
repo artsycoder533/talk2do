808adc4403f52ac99c2c6e2378ba21d7c2be3771
Natasha <njohnson533@gmail.com>
2021-10-13 11:44:36 -0400

    added unitUI function and createElementWithClass function

--- script.js
+++ script.js
@@ -2,6 +2,24 @@ const checkbox = document.getElementById("checkbox");
 const toggleBtn = document.querySelector(".talk");
 const todoItem = document.querySelector(".todos__item");
 const todoInput = document.querySelector(".todos__input");
+const containter = document.querySelector(".todos__container");
+const modal = document.querySelector(".modal"); 
+const talkBtn = document.querySelector(".talk");
+
+
+window.addEventListener("onload", initUI);
+
+function initUI() {
+    
+}
+
+function createElementWithClass(type, className) {
+    const element = document.createElement(type);
+    element.classList.add(className);
+    return element;
+}
+
+
 
 checkbox.addEventListener("click", () => {
 	//change theme of website
@@ -10,3 +28,4 @@ checkbox.addEventListener("click", () => {
     todoItem.classList.toggle("dark");
     todoInput.classList.toggle("dark");
 });
+
