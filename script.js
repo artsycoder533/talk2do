ca3fff42456df67881dd52ddf511b4dbe2529910
Natasha <njohnson533@gmail.com>
2021-10-13 00:49:33 -0400

    added dark mode styling

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const checkbox = document.getElementById("checkbox");
 const toggleBtn = document.querySelector(".talk");
+const todoItem = document.querySelector(".todos__item");
+const todoInput = document.querySelector(".todos__input");
 
 checkbox.addEventListener("click", () => {
 	//change theme of website
 	document.body.classList.toggle("dark");
-	toggleBtn.classList.toggle("dark");
+    toggleBtn.classList.toggle("dark");
+    todoItem.classList.toggle("dark");
+    todoInput.classList.toggle("dark");
 });
